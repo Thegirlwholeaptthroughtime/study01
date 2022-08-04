b5ed9b84dbee1b2495eafd7d292929dbee4799be
dh kang <kdh516094@gmail.com>
2022-08-04 09:53:54 +0900

    test

--- src/jsx/layout/SlideSection.jsx
+++ src/jsx/layout/SlideSection.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
-import 'swiper/css';
 
 
 function SlideSection() {
